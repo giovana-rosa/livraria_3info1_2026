0a1620b2046d352dc596f0c41d2c58c824d8216d
giovana-rosa <giovana.rosaifc@gmail.com>
2026-03-12 11:21:21 -0300

    FEAT: mudança de nomenclatura: "autors" -> "autores".

--- core/models/autor.py
+++ core/models/autor.py
@@ -7,3 +7,7 @@ class Autor(models.Model):
 
     def __str__(self):
         return f"({self.id}) {self.nome}"
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
